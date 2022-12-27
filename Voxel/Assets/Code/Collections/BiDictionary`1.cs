620eee2b6a64b580a8584e3a1e0a1505a31a8a89
Atrufulgium <atrufulgium@gmail.com>
2022-12-27 12:25:37 +0100

    A BiDictionary<T> / BiDictionary<T1,T2> class
    Untested because "I'm just implementing it via the containing dictionaries, what could go wrong?" which will inevitably go wrong.
    * A bi-directional dictionary that can index both by key and by value.

---
+++ Voxel/Assets/Code/Collections/BiDictionary`1.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Atrufulgium.Voxel.Collections {
+    /// <summary>
+    /// Represents a dictionary with two-way O(1) lookup. The standard
+    /// dictionary methods here have variants for the other direction,
+    /// usually denoted by <tt>Reversed</tt>, but sometimes with
+    /// <tt>key</tt> and <tt>value</tt> swapped in the method names.
+    /// </summary>
+    /// <remarks>
+    /// If you want the keys and values to be the different, use the two
+    /// generic parameter variant <see cref="BiDictionary{T1, T2}"/>.
+    /// </remarks>
+    public class BiDictionary<T> :
+        ICollection<KeyValuePair<T, T>>,
+        IDictionary<T, T>,
+        IEnumerable<KeyValuePair<T, T>>,
+        IReadOnlyCollection<KeyValuePair<T, T>> {
+
+        readonly Dictionary<T, T> forward;
+        readonly Dictionary<T, T> backward;
+
+        public BiDictionary() {
+            forward = new();
+            backward = new();
+        }
+        public BiDictionary(IEnumerable<KeyValuePair<T, T>> dictionary) {
+            forward = new(dictionary);
+            backward = new(forward.Count);
+            foreach (var kv in dictionary)
+                backward.Add(kv.Value, kv.Key);
+        }
+        public BiDictionary(IDictionary<T, T> dictionary)
+            : this((IEnumerable<KeyValuePair<T, T>>)dictionary) { }
+        public BiDictionary(int capacity) {
+            forward = new(capacity);
+            backward = new(capacity);
+        }
+        public BiDictionary(IEqualityComparer<T> keyComparer) {
+            forward = new(keyComparer);
+            backward = new();
+        }
+        public BiDictionary(IEqualityComparer<T> keyComparer, IEqualityComparer<T> valueComparer) {
+            forward = new(keyComparer);
+            backward = new(valueComparer);
+        }
+        public BiDictionary(BiDictionary<T> old) {
+            forward = new(old.forward);
+            backward = new(old.backward);
+        }
+
+        T IDictionary<T,T>.this[T key] {
+            get => GetFromKey(key);
+            set => SetFromKey(key, value);
+        }
+        /// <summary> Indexes the bidictionary from the first entry. </summary>
+        public T GetFromKey(T key) => forward[key];
+        /// <inheritdoc cref="GetFromKey(T)"/>
+        public void SetFromKey(T key, T value) {
+            forward[key] = value;
+            backward[value] = key;
+        }
+        /// <summary> Indexes the bidictionary from the second entry. </summary>
+        public T GetFromValue(T value) => backward[value];
+        /// <inheritdoc cref="GetFromValue(T)"/>
+        public void SetFromValue(T value, T key)
+            => SetFromKey(key, value);
+
+        public int Count => forward.Count;
+
+        bool ICollection<KeyValuePair<T, T>>.IsReadOnly => ((ICollection<KeyValuePair<T, T>>)forward).IsReadOnly;
+
+        public ICollection<T> Keys => forward.Keys;
+
+        public ICollection<T> Values => forward.Values;
+
+        public void Add(KeyValuePair<T, T> keyvalue) {
+            ((ICollection<KeyValuePair<T, T>>)forward).Add(keyvalue);
+            ((ICollection<KeyValuePair<T, T>>)backward).Add(new(keyvalue.Value, keyvalue.Key));
+        }
+        public void AddReversed(KeyValuePair<T, T> valuekey)
+            => Add(new KeyValuePair<T, T>(valuekey.Value, valuekey.Key));
+
+        public void Add(T key, T value) {
+            forward.Add(key, value);
+            backward.Add(value, key);
+        }
+        public void AddReversed(T value, T key)
+            => Add(key, value);
+
+        public void Clear() {
+            forward.Clear();
+            backward.Clear();
+        }
+
+        public bool Contains(KeyValuePair<T, T> keyvalue) {
+            return ((ICollection<KeyValuePair<T, T>>)forward).Contains(keyvalue);
+        }
+        public bool ContainsReversed(KeyValuePair<T, T> valuekey)
+            => Contains(new KeyValuePair<T, T>(valuekey.Value, valuekey.Key));
+
+        public bool ContainsKey(T key)
+            => forward.ContainsKey(key);
+        public bool ContainsValue(T value)
+            => backward.ContainsKey(value);
+
+        public void CopyTo(KeyValuePair<T, T>[] array, int arrayIndex)
+            => ((ICollection<KeyValuePair<T, T>>)forward).CopyTo(array, arrayIndex);
+        public void CopyToReversed(KeyValuePair<T, T>[] array, int arrayIndex)
+            => ((ICollection<KeyValuePair<T, T>>)backward).CopyTo(array, arrayIndex);
+
+        public IEnumerator<KeyValuePair<T, T>> GetEnumerator()
+            => forward.GetEnumerator();
+
+        public bool Remove(KeyValuePair<T, T> keyvalue) {
+            ((ICollection<KeyValuePair<T, T>>)backward).Remove(new(keyvalue.Value, keyvalue.Key));
+            return ((ICollection<KeyValuePair<T, T>>)forward).Remove(keyvalue);
+        }
+        public bool RemoveReversed(KeyValuePair<T, T> valuekey)
+            => Remove(new KeyValuePair<T, T>(valuekey.Value, valuekey.Key));
+
+        bool IDictionary<T, T>.Remove(T key)
+            => RemoveByKey(key);
+        public bool RemoveByKey(T key) {
+            if (forward.TryGetValue(key, out T value)) {
+                forward.Remove(key);
+                backward.Remove(value);
+                return true;
+            }
+            return false;
+        }
+        public bool RemoveByValue(T value) {
+            if (backward.TryGetValue(value, out T key)) {
+                backward.Remove(value);
+                forward.Remove(key);
+                return true;
+            }
+            return false;
+        }
+
+        public bool TryGetValue(T key, out T value)
+            => forward.TryGetValue(key, out value);
+        public bool TryGetKey(T value, out T key)
+            => backward.TryGetValue(value, out key);
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return ((IEnumerable)forward).GetEnumerator();
+        }
+    }
+}
---
+++ Voxel/Assets/Code/Collections/BiDictionary`2.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Atrufulgium.Voxel.Collections {
+    /// <summary>
+    /// Represents a dictionary with two-way O(1) lookup. The standard
+    /// dictionary methods here have overloads for *both* generic types.
+    /// </summary>
+    /// <remarks>
+    /// If you want both keys and values to be the same, use the single-generic
+    /// parameter variant, <see cref="BiDictionary{T}"/>. Otherwise, all
+    /// methods here get incredibly awkward to call.
+    /// </remarks>
+    public class BiDictionary<T1, T2> :
+        ICollection<KeyValuePair<T1, T2>>,
+        IDictionary<T1, T2>,
+        IEnumerable<KeyValuePair<T1, T2>>,
+        IReadOnlyCollection<KeyValuePair<T1, T2>> {
+
+        readonly Dictionary<T1, T2> forward;
+        readonly Dictionary<T2, T1> backward;
+
+        public BiDictionary() {
+            forward = new();
+            backward = new();
+        }
+        public BiDictionary(IEnumerable<KeyValuePair<T1, T2>> dictionary) {
+            forward = new(dictionary);
+            backward = new(forward.Count);
+            foreach (var kv in dictionary)
+                backward.Add(kv.Value, kv.Key);
+        }
+        public BiDictionary(IDictionary<T1, T2> dictionary)
+            : this((IEnumerable<KeyValuePair<T1, T2>>)dictionary) { }
+        public BiDictionary(IEnumerable<KeyValuePair<T2, T1>> dictionary) {
+            backward = new(dictionary);
+            forward = new(backward.Count);
+            foreach (var kv in dictionary)
+                forward.Add(kv.Value, kv.Key);
+        }
+        public BiDictionary(IDictionary<T2, T1> dictionary)
+            : this((IEnumerable<KeyValuePair<T2, T1>>)dictionary) { }
+        public BiDictionary(int capacity) {
+            forward = new(capacity);
+            backward = new(capacity);
+        }
+        public BiDictionary(IEqualityComparer<T1> t1Comparer) {
+            forward = new(t1Comparer);
+            backward = new();
+        }
+        public BiDictionary(IEqualityComparer<T2> t2Comparer) {
+            forward = new();
+            backward = new(t2Comparer);
+        }
+        public BiDictionary(IEqualityComparer<T1> t1Comparer, IEqualityComparer<T2> t2Comparer) {
+            forward = new(t1Comparer);
+            backward = new(t2Comparer);
+        }
+        public BiDictionary(BiDictionary<T1,T2> old) {
+            forward = new(old.forward);
+            backward = new(old.backward);
+        }
+
+        public T2 this[T1 key] {
+            get => forward[key];
+            set {
+                forward[key] = value;
+                backward[value] = key;
+            }
+        }
+
+        public T1 this[T2 key] {
+            get => backward[key];
+            set {
+                backward[key] = value;
+                forward[value] = key;
+            }
+        }
+
+        public int Count => forward.Count;
+
+        bool ICollection<KeyValuePair<T1,T2>>.IsReadOnly => ((ICollection<KeyValuePair<T1, T2>>)forward).IsReadOnly;
+
+        public ICollection<T1> Keys => forward.Keys;
+
+        public ICollection<T2> Values => forward.Values;
+
+        public void Add(KeyValuePair<T1, T2> item) {
+            ((ICollection<KeyValuePair<T1, T2>>)forward).Add(item);
+            ((ICollection<KeyValuePair<T2, T1>>)backward).Add(new(item.Value, item.Key));
+        }
+        public void Add(KeyValuePair<T2, T1> item)
+            => Add(new KeyValuePair<T1, T2>(item.Value, item.Key));
+
+        public void Add(T1 key, T2 value) {
+            forward.Add(key, value);
+            backward.Add(value, key);
+        }
+        public void Add(T2 key, T1 value)
+            => Add(value, key);
+
+        public void Clear() {
+            forward.Clear();
+            backward.Clear();
+        }
+
+        public bool Contains(KeyValuePair<T1, T2> item) {
+            return ((ICollection<KeyValuePair<T1, T2>>)forward).Contains(item);
+        }
+        public bool Contains(KeyValuePair<T2, T1> item)
+            => Contains(new KeyValuePair<T1, T2>(item.Value, item.Key));
+
+        public bool ContainsKey(T1 key)
+            => forward.ContainsKey(key);
+        public bool ContainsKey(T2 key)
+            => backward.ContainsKey(key);
+
+        public void CopyTo(KeyValuePair<T1, T2>[] array, int arrayIndex)
+            => ((ICollection<KeyValuePair<T1, T2>>)forward).CopyTo(array, arrayIndex);
+        public void CopyTo(KeyValuePair<T2, T1>[] array, int arrayIndex)
+            => ((ICollection<KeyValuePair<T2, T1>>)backward).CopyTo(array, arrayIndex);
+
+        public IEnumerator<KeyValuePair<T1, T2>> GetEnumerator()
+            => forward.GetEnumerator();
+
+        public bool Remove(KeyValuePair<T1, T2> item) {
+            ((ICollection<KeyValuePair<T2, T1>>)backward).Remove(new(item.Value, item.Key));
+            return ((ICollection<KeyValuePair<T1, T2>>)forward).Remove(item);
+        }
+        public bool Remove(KeyValuePair<T2, T1> item)
+            => Remove(new KeyValuePair<T1, T2>(item.Value, item.Key));
+
+        public bool Remove(T1 key) {
+            if (forward.TryGetValue(key, out T2 value)) {
+                forward.Remove(key);
+                backward.Remove(value);
+                return true;
+            }
+            return false;
+        }
+        public bool Remove(T2 key) {
+            if (backward.TryGetValue(key, out T1 value)) {
+                backward.Remove(key);
+                forward.Remove(value);
+                return true;
+            }
+            return false;
+        }
+
+        public bool TryGetValue(T1 key, out T2 value)
+            => forward.TryGetValue(key, out value);
+        public bool TryGetValue(T2 key, out T1 value)
+            => backward.TryGetValue(key, out value);
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return ((IEnumerable)forward).GetEnumerator();
+        }
+    }
+}
